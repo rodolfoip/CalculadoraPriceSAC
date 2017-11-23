de188ed7883ddd647d6cd6a1f8ceee27f6b37ec3
rodolfo <rodolfo.pereirailce@gmail.com>
2017-11-23 15:31:43 -0200

    Tabela do calculo price OK

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,4 +1,28 @@
-function validarCampos() {
+function validarValoresCampos() {
+    var valor = document.getElementById("valorCampo").value;
+    var nParcelas = document.getElementById("nParcelasCampo").value;
+    var entrada = document.getElementById("entradaCampo").value;
+    var juros = document.getElementById("jurosCampo").valueAsNumber;
+
+    if (valor < "0") {
+        document.getElementById("valorCampo").value = "0";
+        return false;
+    }
+    if (nParcelas < "0") {
+        document.getElementById("nParcelasCampo").value = "0";
+        return false;
+    }
+    if (entrada < "0") {
+        document.getElementById("entradaCampo").value = "0";
+        return false;
+    }
+    if (juros < 0.00) {
+        document.getElementById("jurosCampo").value = 0.00;
+        return false;
+    }
+}
+
+function validarCamposVazios() {
     var valor = document.getElementById("valorCampo").value;
     var nParcelas = document.getElementById("nParcelasCampo").value;
     var entrada = document.getElementById("entradaCampo").value;
@@ -7,18 +31,22 @@ function validarCampos() {
     if (valor == "") {
         document.getElementById("valorCampo").focus();
         return false;
-    } else if (entrada == "") {
+    }
+    if (entrada == "") {
         document.getElementById("entradaCampo").focus();
         return false;
-    } else if (nParcelas == "") {
+    }
+    if (nParcelas == "") {
         document.getElementById("nParcelasCampo").focus();
         return false;
-    } else if (!juros) {
+    }
+    if (juros<0) {
         document.getElementById("jurosCampo").focus();
         return false;
     }
     return true;
 }
+
 
 function calculaParcelaPrice(valorPresente, nParcelas, entrada, juros) {
     //conta para saber valor prestação
@@ -28,7 +56,7 @@ function calculaParcelaPrice(valorPresente, nParcelas, entrada, juros) {
 }
 
 function calculaPrice() {
-    if (validarCampos() == true) {
+    if (validarCamposVazios()) {
         var valor = document.getElementById("valorCampo").value;
         var nParcelas = document.getElementById("nParcelasCampo").value;
         var entrada = document.getElementById("entradaCampo").value;
@@ -44,39 +72,53 @@ function criaTabelaPrice(valor, parcela, nParcelas, juros) {
     //verifica se existe tabela, caso sim, apaga
     if (document.getElementById("tabela").childElementCount != 0) {
         console.log("apagou tabela");
-        document.getElementById("tabela").innerHTML = ""
+        document.getElementById("tabela").innerHTML = "";
     }
+    desocultatabela();
     //TABELA
     var corpo = document.getElementById("tabela");
 
     var tabela = document.createElement("table");
     var corpoTabela = document.createElement("tbody");
     var headerTabela = document.createElement("thead");
+    tabela.setAttribute("class","table");
+    headerTabela.setAttribute("class", "thead-dark");
     var colunas = ["Nº", "Valor parcela", "Juros", "Amortização", "Saldo devedor"];
-
-
-    for (var i = 0; i < 1; i++) {
-        var linha = document.createElement("tr");
-        for (var k = 0; k < 5; k++) {
-            var celulaColuna = document.createElement("th");
-            var textoColuna = document.createTextNode(colunas[k]);
-            celulaColuna.appendChild(textoColuna);
-            linha.appendChild(celulaColuna);
-            headerTabela.appendChild(linha);
-        }
+    var celulaColuna;
+    var textoColuna;
+    var linha = document.createElement("tr");
+    for (var i = 0; i < 5; i++) {
+        celulaColuna = document.createElement("th");
+        textoColuna = document.createTextNode(colunas[i]);
+        celulaColuna.appendChild(textoColuna);
+        linha.appendChild(celulaColuna);
+        headerTabela.appendChild(linha);
+        celulaColuna.setAttribute("class","col");
     }
-
     for (var j = 0; j <= nParcelas; j++) {
         if (j == 0) {
-            var linha = document.createElement("tr");
-            var celula = document.createElement("td");
+            linha = document.createElement("tr");
+            var celula1 = document.createElement("td");
             var texto1 = document.createTextNode(j);
+            celula1.appendChild(texto1);
+            linha.appendChild(celula1);
+            var celula2 = document.createElement("td");
+            var texto2 = document.createTextNode(valor);
+            celula2.appendChild(texto2);
+            linha.appendChild(celula2);
+            var celula3 = document.createElement("td");
+            var texto3 = document.createTextNode(0);
+            celula3.appendChild(texto3);
+            linha.appendChild(celula3);
+            var celula4 = document.createElement("td");
+            var texto4 = document.createTextNode(0);
+            celula4.appendChild(texto4);
+            linha.appendChild(celula4);
             var celula5 = document.createElement("td");
             var texto5 = document.createTextNode(valor);
-            celula.appendChild(texto1);
-            linha.appendChild(celula);
             celula5.appendChild(texto5);
             linha.appendChild(celula5);
+
             corpoTabela.appendChild(linha);
         } else {
 
@@ -84,7 +126,7 @@ function criaTabelaPrice(valor, parcela, nParcelas, juros) {
             var amortizacao = parcela - totalJuros;
             valor = valor - amortizacao;
 
-            var linha = document.createElement("tr");
+            linha = document.createElement("tr");
             var celula1 = document.createElement("td");
             var texto1 = document.createTextNode(j);
             celula1.appendChild(texto1);
@@ -115,22 +157,24 @@ function criaTabelaPrice(valor, parcela, nParcelas, juros) {
 }
 
 function calculaSAC() {
-    var valor = document.getElementById("valorCampo").value;
-    var nParcelas = document.getElementById("nParcelasCampo").value;
-    var entrada = document.getElementById("entradaCampo").value;
-    var juros = document.getElementById("jurosCampo").valueAsNumber;
-    juros = (juros / 100);
-    valor = (valor - entrada);
-    var amortizacao = (valor / nParcelas);
+    if (validarCamposVazios() == true) {
+        var valor = document.getElementById("valorCampo").value;
+        var nParcelas = document.getElementById("nParcelasCampo").value;
+        var entrada = document.getElementById("entradaCampo").value;
+        var juros = document.getElementById("jurosCampo").valueAsNumber;
+        juros = (juros / 100);
+        valor = (valor - entrada);
+        var amortizacao = (valor / nParcelas);
 
-    criarTabelaSAC(valor, juros, amortizacao, nParcelas);
+        criarTabelaSAC(valor, juros, amortizacao, nParcelas);
+    }
 }
 
 function criarTabelaSAC(valor, juros, amortizacao, nParcelas) {
     //verifica se existe tabela, caso sim, apaga
     if (document.getElementById("tabela").childElementCount != 0) {
         console.log("apagou tabela");
-        document.getElementById("tabela").innerHTML = ""
+        document.getElementById("tabela").innerHTML = "";
     }
     //TABELA
     var corpo = document.getElementById("tabela");
@@ -140,24 +184,38 @@ function criarTabelaSAC(valor, juros, amortizacao, nParcelas) {
     var headerTabela = document.createElement("thead");
     var colunas = ["Nº", "Valor parcela", "Juros", "Amortização", "Saldo devedor"];
 
-    for (var i = 0; i < 1; i++) {
-        var linha = document.createElement("tr");
-        for (var k = 0; k < 5; k++) {
-            var celulaColuna = document.createElement("th");
-            var textoColuna = document.createTextNode(colunas[k]);
-            celulaColuna.appendChild(textoColuna);
-            linha.appendChild(celulaColuna);
-            headerTabela.appendChild(linha);
-        }
+    var linha = document.createElement("tr");
+    for (var k = 0; k < 5; k++) {
+        var celulaColuna = document.createElement("th");
+        var textoColuna = document.createTextNode(colunas[k]);
+        celulaColuna.appendChild(textoColuna);
+        linha.appendChild(celulaColuna);
+        headerTabela.appendChild(linha);
     }
 
     for (var j = 0; j <= nParcelas; j++) {
         if (j == 0) {
-            var linha = document.createElement("tr");
-            var celula = document.createElement("td");
+            linha = document.createElement("tr");
+            var celula1 = document.createElement("td");
             var texto1 = document.createTextNode(j);
-            celula.appendChild(texto1);
-            linha.appendChild(celula);
+            celula1.appendChild(texto1);
+            linha.appendChild(celula1);
+            var celula2 = document.createElement("td");
+            var texto2 = document.createTextNode(valor);
+            celula2.appendChild(texto2);
+            linha.appendChild(celula2);
+            var celula3 = document.createElement("td");
+            var texto3 = document.createTextNode(0);
+            celula3.appendChild(texto3);
+            linha.appendChild(celula3);
+            var celula4 = document.createElement("td");
+            var texto4 = document.createTextNode(0);
+            celula4.appendChild(texto4);
+            linha.appendChild(celula4);
+            var celula5 = document.createElement("td");
+            var texto5 = document.createTextNode(valor);
+            celula5.appendChild(texto5);
+            linha.appendChild(celula5);
             corpoTabela.appendChild(linha);
         } else {
             var totalJuros = (valor * juros);
@@ -193,11 +251,16 @@ function criarTabelaSAC(valor, juros, amortizacao, nParcelas) {
     tabela.appendChild(corpoTabela);
     corpo.appendChild(tabela);
 }
+
 function ocultabotaoprice() {
     document.getElementById("btnprice").style.display = "none";
     document.getElementById("btnsac").style.display = "flex";
 }
+
 function ocultabotaosac() {
     document.getElementById("btnsac").style.display = "none";
     document.getElementById("btnprice").style.display = "flex";
+}
+function desocultatabela() {
+    document.getElementById("tabela").style.display = "flex";
 }
